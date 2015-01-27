00a3bf7a06e5802d7eb64c3feda426a281c7cfe0
Joshua Downey <downey.josh@gmail.com>
2015-01-26 17:36:47 -0800

    Removed jumbotron

--- page-homepage.php
+++ page-homepage.php
@@ -16,18 +16,7 @@ Template Name: Homepage
 
 						<header>
 
-							<?php
-								$post_thumbnail_id = get_post_thumbnail_id();
-								$featured_src = wp_get_attachment_image_src( $post_thumbnail_id, 'jwdmc-featured-home' );
-							?>
-
-							<div class="jumbotron" style="background-image: url('<?php echo $featured_src[0]; ?>'); background-repeat: no-repeat; background-position: 0 0;">
-
-								<div class="page-header">
-									<h1><?php bloginfo('title'); ?></h1>
-								</div>
-
-							</div>
+								<div class="page-header"><h1><?php bloginfo('title'); ?></h1></div>
 
 						</header>
 
