aee6d1137bc20741c1bf9c5dd15004f1708119d3
Joshua Downey <downey.josh@gmail.com>
2016-07-29 10:25:13 -0700

    fix function prefixes

--- functions.php
+++ functions.php
@@ -20,12 +20,14 @@ require_once('lib/tgm-plugin-activation.php'); // this one too
 
 
 // Set content width
-if ( ! isset( $content_width ) ) $content_width = 580;
+if ( ! isset( $content_width ) ) {
+	$content_width = 1140;
+}
 
 
 // Thumbnail sizes
 add_image_size( 'jwdmc-featured', 780, 400, true );
-add_image_size( 'jwdmc-gallery-thumbnail', 250, 250, true);
+add_image_size( 'jwdmc-gallery-thumbnail', 250, 250, true );
 
 
 // Sidebars & Widgetizes Areas
@@ -76,7 +78,7 @@ function jwdmc_register_sidebars() {
 
 
 // Register the required plugins for this theme.
-function my_theme_register_required_plugins() {
+function jwdmc_register_required_plugins() {
 
 	$plugins = array(
 		array(
@@ -150,7 +152,7 @@ function my_theme_register_required_plugins() {
 	tgmpa( $plugins, $config );
 
 }
-add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
+add_action( 'tgmpa_register', 'jwdmc_register_required_plugins' );
 
 
 // Comment Layout
@@ -194,7 +196,7 @@ function list_pings( $comment, $args, $depth ) {
 
 
 // password protected post form
-function custom_password_form() {
+function jwdmc_custom_password_form() {
 	global $post;
 	$label = 'pwbox-'.( empty( $post->ID ) ? rand() : $post->ID );
 	$o = '<div class="clearfix"><form class="protected-post-form" action="' . get_option('siteurl') . '/wp-login.php?action=postpass" method="post">
@@ -204,22 +206,22 @@ function custom_password_form() {
 	';
 	return $o;
 }
-add_filter( 'the_password_form', 'custom_password_form' );
+add_filter( 'the_password_form', 'jwdmc_custom_password_form' );
 
 
 // update standard wp tag cloud widget so it looks better
-function my_widget_tag_cloud_args( $args ) {
+function jwdmc_widget_tag_cloud_args( $args ) {
 	$args['number'] = 20; // show less tags
 	$args['largest'] = 9.75; // make largest and smallest the same - i don't like the varying font-size look
 	$args['smallest'] = 9.75;
 	$args['unit'] = 'px';
 	return $args;
 }
-add_filter( 'widget_tag_cloud_args', 'my_widget_tag_cloud_args' );
+add_filter( 'widget_tag_cloud_args', 'jwdmc_widget_tag_cloud_args' );
 
 
 // filter tag clould output so that it can be styled by CSS
-function add_tag_class( $taglinks ) {
+function jwdmc_add_tag_class( $taglinks ) {
 	$tags = explode('</a>', $taglinks);
 	$regex = "#(.*tag-link[-])(.*)(' title.*)#e";
 
@@ -231,12 +233,12 @@ function add_tag_class( $taglinks ) {
 
 	return $taglinks;
 }
-add_action( 'wp_tag_cloud', 'add_tag_class' );
+add_action( 'wp_tag_cloud', 'jwdmc_add_tag_class' );
 
-function wp_tag_cloud_filter( $return, $args ) {
+function jwdmc_tag_cloud_filter( $return, $args ) {
 	return '<div id="tag-cloud">' . $return . '</div>';
 }
-add_filter( 'wp_tag_cloud','wp_tag_cloud_filter', 10, 2) ;
+add_filter( 'wp_tag_cloud','jwdmc_tag_cloud_filter', 10, 2) ;
 
 
 // Enable shortcodes in widgets
@@ -244,7 +246,7 @@ add_filter( 'widget_text', 'do_shortcode' );
 
 
 // Disable jump in 'read more' link
-function remove_more_jump_link( $link ) {
+function jwdmc_remove_more_jump_link( $link ) {
 	$offset = strpos( $link, '#more-' );
 	if ( $offset ) {
 		$end = strpos( $link, '"',$offset );
@@ -254,25 +256,25 @@ function remove_more_jump_link( $link ) {
 	}
 	return $link;
 }
-add_filter( 'the_content_more_link', 'remove_more_jump_link' );
+add_filter( 'the_content_more_link', 'jwdmc_remove_more_jump_link' );
 
 
 // Remove height/width attributes on images so they can be responsive
-function remove_thumbnail_dimensions( $html ) {
+function jwdmc_remove_thumbnail_dimensions( $html ) {
 	$html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
 	return $html;
 }
-add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
-add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
+add_filter( 'post_thumbnail_html', 'jwdmc_remove_thumbnail_dimensions', 10 );
+add_filter( 'image_send_to_editor', 'jwdmc_remove_thumbnail_dimensions', 10 );
 
 
 // Add thumbnail class to thumbnail links
-function add_class_attachment_link( $html ) {
+function jwdmc_add_class_attachment_link( $html ) {
 	$postid = get_the_ID();
 	$html = str_replace( '<a','<a class="thumbnail"',$html );
 	return $html;
 }
-add_filter( 'wp_get_attachment_link', 'add_class_attachment_link', 10, 1 );
+add_filter( 'wp_get_attachment_link', 'jwdmc_add_class_attachment_link', 10, 1 );
 
 
 // Menu output mods
@@ -341,13 +343,13 @@ class Bootstrap_walker extends Walker_Nav_Menu {
 
 
 // Add Bootstrap's standard 'active' class name to the active nav link item
-function add_active_class( $classes, $item ) {
+function jwdmc_add_active_class( $classes, $item ) {
 	if( $item->menu_item_parent == 0 && in_array('current-menu-item', $classes) ) {
 		$classes[] = "active";
 	}
 	return $classes;
 }
-add_filter('nav_menu_css_class', 'add_active_class', 10, 2 );
+add_filter('nav_menu_css_class', 'jwdmc_add_active_class', 10, 2 );
 
 
 // Load editor stylesheet
@@ -395,7 +397,7 @@ if( !function_exists("theme_styles") ) {
 		wp_register_style( 'jwdmc-style',
 			get_stylesheet_directory_uri() . '/css/main.min.css',
 			array(),
-			'2.2.0',
+			'2.3.0',
 			'all' );
 		wp_enqueue_style( 'jwdmc-style' );
 	}
@@ -425,7 +427,7 @@ if( !function_exists( "theme_js" ) ) {
 		wp_register_script( 'jwdmc-scripts',
 			get_template_directory_uri() . '/js/main.min.js',
 			array('jquery'),
-			'2.2.0',
+			'2.3.0',
 			true );
 		wp_enqueue_script('jwdmc-scripts');
 	}
@@ -440,7 +442,7 @@ add_action( 'wp_enqueue_scripts', 'theme_js' );
  * @param string $sep Optional separator.
  * @return string The filtered title.
  */
-function framework_wp_title( $title, $sep ) {
+function jwdmc_framework_wp_title( $title, $sep ) {
 	if ( is_feed() ) {
 		return $title;
 	}
@@ -463,7 +465,7 @@ function framework_wp_title( $title, $sep ) {
 
 	return $title;
 }
-add_filter( 'wp_title', 'framework_wp_title', 10, 2 );
+add_filter( 'wp_title', 'jwdmc_framework_wp_title', 10, 2 );
 
 
 // Custom Backend Footer
@@ -474,27 +476,27 @@ add_filter('admin_footer_text', 'jwdmc_custom_admin_footer');
 
 
 // Custom WordPress login screen
-function my_login_logo_url() {
+function jwdmc_login_logo_url() {
 	return get_bloginfo( 'url' );
 }
-add_filter( 'login_headerurl', 'my_login_logo_url' );
+add_filter( 'login_headerurl', 'jwdmc_login_logo_url' );
 
-function my_login_logo_url_title() {
+function jwdmc_login_logo_url_title() {
 	return 'Created by Jennifer Web Design';
 }
-add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+add_filter( 'login_headertitle', 'jwdmc_login_logo_url_title' );
 
-function my_login_stylesheet() { ?>
+function jwdmc_login_stylesheet() { ?>
 	<link rel="stylesheet" id="custom_wp_admin_css"  href="<?php echo get_bloginfo( 'stylesheet_directory' ) . '/css/login.css'; ?>" type="text/css" media="all" />
 <?php }
-add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+add_action( 'login_enqueue_scripts', 'jwdmc_login_stylesheet' );
 
 
 // Put Yoast meta box at bottom of screen by default
-function yoasttobottom() {
+function jwdmc_yoasttobottom() {
 	return 'low';
 }
-add_filter( 'wpseo_metabox_prio', 'yoasttobottom');
+add_filter( 'wpseo_metabox_prio', 'jwdmc_yoasttobottom');
 
 
 /**
@@ -504,32 +506,32 @@ add_filter( 'wpseo_metabox_prio', 'yoasttobottom');
  * @param string $form The form the button is tied to.
  * @return string The filtered button.
  */
-function form_submit_button( $button, $form ) {
+function jwdmc_form_submit_button( $button, $form ) {
 	$button = str_replace( 'input', 'button', $button );
 	$button = str_replace( '/', '', $button );
 	$button = str_replace( 'gform_button button', 'btn btn-primary', $button );
 	$button .= "{$form['button']['text']}</button>";
 	return $button;
 }
-add_filter( 'gform_submit_button', 'form_submit_button', 10, 5 );
+add_filter( 'gform_submit_button', 'jwdmc_form_submit_button', 10, 5 );
 
-function form_next_button( $button, $form ) {
+function jwdmc_form_next_button( $button, $form ) {
 	$button = str_replace( 'input', 'button', $button );
 	$button = str_replace( '/', '', $button );
 	$button = str_replace( 'gform_next_button button', 'btn btn-default', $button );
 	$button .= 'Next</button>';
 	return $button;
 }
-add_filter( 'gform_next_button', 'form_next_button', 10, 5 );
+add_filter( 'gform_next_button', 'jwdmc_form_next_button', 10, 5 );
 
-function form_previous_button( $button, $form ) {
+function jwdmc_form_previous_button( $button, $form ) {
 	$button = str_replace( 'input', 'button', $button );
 	$button = str_replace( '/', '', $button );
 	$button = str_replace( 'gform_previous_button button', 'btn btn-default', $button );
 	$button .= 'Previous</button>';
 	return $button;
 }
-add_filter( 'gform_previous_button', 'form_previous_button', 10, 5 );
+add_filter( 'gform_previous_button', 'jwdmc_form_previous_button', 10, 5 );
 
 
 ?>
